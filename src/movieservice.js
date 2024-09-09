fbb7c7234d75e81744fbb308817fcbecf4e24120
Nilvany Sunguessungue <tiagonilvany@gmail.com>
2024-09-09 19:14:54 +0100

    update config.js and movieservice.js

--- src/movieservice.js
+++ src/movieservice.js
@@ -1,16 +1,29 @@
 const axios = require('axios')
-//import axios module
 
-//After starting the JSOn server check the port on which is running accordingly change 
-//the port in url given below
+const sendLog = async (level, message, details) => {
+  const log = {
+    level: level,
+    message: message,
+    details: details
+  }
 
-//This method will get all movies from json server
+  try {
+    await axios.post('http://localhost:8000/logs', log)
+    console.log('Log send successfuly')
+  } catch (err) {
+    console.error('Falha ao enviar log', err)
+  }
+}
 const getMovies = (done) => {
   // This url can be used - axios.get("http://localhost:3000/movies")
   axios.get("http://localhost:3000/movies").then(response => {
     const movies = response.data
+
+    sendLog('INFO', 'Get movies successfuly', { count: movies.length })
+
     return done(undefined, movies)
   }).catch(error => {
+    sendLog('ERROR', 'Error get movies', { error: error.message })
     done("Encountered error while getting movies details ", error)
   })
 }
@@ -20,8 +33,10 @@ const getMovieById = (movieId, done) => {
   // This url can be used- axios.get(`http://localhost:3000/movies/${movieId}`)
   axios.get(`http://localhost:3000/movies/${movieId}`).then(response => {
     const movie = response.data
+    sendLog('INFO', `movie with ID ${movieId} find with success`, { movieId })
     return done(undefined, movie)
   }).catch(error => {
+    sendLog('ERROR', `Error to return movie with ${movieId}`, { error: error.message })
     done("Encountered error while getting movies details ", error)
   })
 }
@@ -41,8 +56,13 @@ const updateMovieDetails = (movieId, movieDetails, done) => {
   //This url can be used - axios.patch(`http://localhost:3000/movies/${movieId}`, movieDetails)
   axios.patch(`http://localhost:3000/movies/${movieId}`, movieDetails).then(response => {
     const movie = response.data
+
+    sendLog('INFO', 'movie saved successfuly', { movie });
+
     return done(undefined, movie)
   }).catch(error => {
+    sendLog('ERROR', 'Error saving movie', { error: error.message });
+
     done("Encountered error while update movies details ", error)
   })
 }
@@ -52,8 +72,10 @@ const deleteMovieById = (movieId, done) => {
   //This url can be used -  axios.delete(`http://localhost:3000/movies/${movieId}`)
   axios.delete(`http://localhost:3000/movies/${movieId}`).then(response => {
     const movie = response.data
+    sendLog('INFO', `movie with ID ${movieId} deleted with success`, { movieId })
     return done(undefined, movie)
   }).catch(error => {
+    sendLog('ERROR', 'Error deleting movie', { error: error.message });
     done("Encountered error while delete movies details ", error)
   })
 }
